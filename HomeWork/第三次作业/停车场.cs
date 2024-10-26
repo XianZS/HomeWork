c2d58b5ee43b82c4c692978f6a54436038fde9a2
XianZS <XianZhiSen_yang@outlook.com>
2024-10-26 13:47:57 +0800

    XianZS

---
+++ HomeWork/第三次作业/停车场.cs
@@ -0,0 +1,95 @@
+﻿namespace HomeWork
+{
+    class 停车场
+    {
+        static void Main(string[] args)
+        {
+            MyStack mystack = new MyStack(6);
+            Console.WriteLine(mystack.JudgeEmpty());
+            Console.WriteLine(mystack.JudgeFull());
+            for(int i = 0; i < 6; i++)
+            {
+                mystack.PushChild(i+1);
+            }
+            Console.WriteLine(mystack.JudgeFull());
+            for(int i = 0;i < 6; i++)
+            {
+                Console.WriteLine(mystack.ShowTopChild());
+            }
+            for(int i = 0;i < 6; i++)
+            {
+                Console.Write("{0} ",mystack.PopChild());
+            }
+        }
+    }
+    class MyStack // 栈
+    {
+        // 栈顶指针
+        private int top = -1;
+        // 栈最大长度
+        private int maxSize;
+        // 栈数组
+        private int[] nums;
+        // 构造器
+        public MyStack(int InputMaxSize)
+        {
+            maxSize = InputMaxSize;
+            nums = new int[maxSize];
+        }
+        public MyStack()
+        {
+            maxSize = 3;
+            nums = new int[maxSize];
+        }
+        // 判断栈是否已满
+        public bool JudgeFull()
+        {
+            if (top == maxSize - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        // 判断栈是否为空
+        public bool JudgeEmpty()
+        {
+            if (top == -1)
+            {
+                return true;
+            }
+            return false;
+        }
+        // 入栈push
+        public bool PushChild(int pushNum)
+        {
+            if (JudgeFull())
+            {
+                Console.WriteLine("栈内元素已满");
+                return false;
+            }
+            top += 1;
+            nums[top] = pushNum;
+            return true;
+        }
+        // 出栈pop
+        public int PopChild()
+        {
+            if (JudgeEmpty())
+            {
+                Console.WriteLine("栈为空");
+                return -1;
+            }
+            return nums[top--];
+        }
+        // 查看当前栈顶元素
+        public int ShowTopChild()
+        {
+            if (JudgeEmpty() || top == -1)
+            {
+                Console.WriteLine("栈为空");
+                return -1;
+            }
+            return nums[top];
+        }
+    }
+}
--- HomeWork/第二次作业/question3.cs
+++ HomeWork/第二次作业/question3.cs
@@ -1,79 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace HomeWork
-{
-    class question3
-    {
-        static void Main(string[] args)
-        {
-            Student[] nums = new Student[3]; // 构建三个示例学生
-            for (int i = 0; i < nums.Length; i++)
-            {
-                nums[i] = new Student();
-                Console.Write("请输入第{0}个学生的学号:", i + 1);
-                nums[i].Id = Convert.ToInt32(Console.ReadLine());
-                Console.Write("请输入第{0}个学生的姓名:", i + 1);
-                nums[i].Name = Console.ReadLine();
-                Console.Write("请依次输入C++、英语和数学成绩，使用空格隔开:");
-                int[] MyGrade = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
-                nums[i].Grade = MyGrade;
-            }
-            for (int i = 0; i < nums.Length; i++)
-            {
-                nums[i].PrintAllThings();
-                Console.WriteLine("总成绩:{0},平均成绩:{1}", nums[i].AllGrade(), nums[i].AveGrade());
-            }
-        }
-    }
-    class Student
-    {
-        private string name;
-        private int id;
-        private int[] grade;
-        public Student() // 无参构造函数
-        {
-            name = "姓名未输入";
-            id = 0;
-            grade = [];
-        }
-        public Student(string InputName, int InputId, int[] InputGrade) // 有参构造函数
-        {
-            name = InputName;
-            id = InputId;
-            grade = InputGrade;
-        }
-        public string Name // 设置姓名
-        {
-            get { return name; }
-            set { name = value; }
-        }
-        public int Id // 设置学号
-        {
-            get { return id; }
-            set { id = value; }
-        }
-        public int[] Grade //设置成绩数组
-        {
-            get { return grade; }
-            set { grade = value; }
-        }
-        public void PrintAllThings() //输出基本信息
-        {
-            Console.WriteLine("{0}学号为{1},C++成绩为:{2},英语成绩为:{3},数学成绩为:{4}", name, id, grade[0], grade[1], grade[2]);
-        }
-        public int AllGrade() // 求总成绩
-        {
-            int sumNum = grade.Sum();
-            return sumNum;
-        }
-        public double AveGrade() //求平均成绩
-        {
-            int sumNum = grade.Sum();
-            return sumNum / 3.0;
-        }
-    }
-}
+//namespace HomeWork
+//{
+//    class question3
+//    {
+//        static void Main(string[] args)
+//        {
+//            Student[] nums = new Student[3]; // 构建三个示例学生
+//            for (int i = 0; i < nums.Length; i++)
+//            {
+//                nums[i] = new Student();
+//                Console.Write("请输入第{0}个学生的学号:", i + 1);
+//                nums[i].Id = Convert.ToInt32(Console.ReadLine());
+//                Console.Write("请输入第{0}个学生的姓名:", i + 1);
+//                nums[i].Name = Console.ReadLine();
+//                Console.Write("请依次输入C++、英语和数学成绩，使用空格隔开:");
+//                int[] MyGrade = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
+//                nums[i].Grade = MyGrade;
+//            }
+//            for (int i = 0; i < nums.Length; i++)
+//            {
+//                nums[i].PrintAllThings();
+//                Console.WriteLine("总成绩:{0},平均成绩:{1}", nums[i].AllGrade(), nums[i].AveGrade());
+//            }
+//        }
+//    }
+//    class Student
+//    {
+//        private string name;
+//        private int id;
+//        private int[] grade;
+//        public Student() // 无参构造函数
+//        {
+//            name = "姓名未输入";
+//            id = 0;
+//            grade = [];
+//        }
+//        public Student(string InputName, int InputId, int[] InputGrade) // 有参构造函数
+//        {
+//            name = InputName;
+//            id = InputId;
+//            grade = InputGrade;
+//        }
+//        public string Name // 设置姓名
+//        {
+//            get { return name; }
+//            set { name = value; }
+//        }
+//        public int Id // 设置学号
+//        {
+//            get { return id; }
+//            set { id = value; }
+//        }
+//        public int[] Grade //设置成绩数组
+//        {
+//            get { return grade; }
+//            set { grade = value; }
+//        }
+//        public void PrintAllThings() //输出基本信息
+//        {
+//            Console.WriteLine("{0}学号为{1},C++成绩为:{2},英语成绩为:{3},数学成绩为:{4}", name, id, grade[0], grade[1], grade[2]);
+//        }
+//        public int AllGrade() // 求总成绩
+//        {
+//            int sumNum = grade.Sum();
+//            return sumNum;
+//        }
+//        public double AveGrade() //求平均成绩
+//        {
+//            int sumNum = grade.Sum();
+//            return sumNum / 3.0;
+//        }
+//    }
+//}
