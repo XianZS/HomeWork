e8348b3f955489078399a88d1ee0dee591a2d908
XianZS <XianZhiSen_yang@outlook.com>
2024-11-01 10:35:02 +0800

    XianZS

---
+++ HomeWork/11月1日/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            A a = new B();
+            a.Pr();
+        }
+    }
+    class A
+    {
+        public virtual void Pr()
+        {
+            Console.WriteLine("中文");
+        }
+    }
+    class B : A
+    {
+        public override void Pr()
+        {
+            Console.WriteLine("英文");
+        }
+    }
+}
